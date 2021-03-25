744d6d163a6187157a2031e4b44a1cc7a7b40e58
asif19981998 <rakibulasif1998@gmail.com>
2021-03-25 13:04:29 +0600

    responsive navber add

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './main-component/home';
 import Navbar from './sub-component/nav-bar';
+
+import Resume from './main-component/resume';
+import Projects from './main-component/projects';
+import Contact from './main-component/contact';
+import { Route } from 'react-router';
 function App() {
   return (
     <div className="body">
       <Navbar></Navbar>
+      <Route path="/home" component={Home}></Route>
+      <Route path="/resume" component={Resume}></Route>
+      <Route path="/projects" component={Projects}></Route>
+      <Route path="/contact" component={Contact}></Route>
     </div>
     
    );
--- src/sub-component/nav-bar.jsx
+++ src/sub-component/nav-bar.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
     constructor(props) {
@@ -7,43 +10,38 @@ class Navbar extends Component {
     }
     render() { 
         return ( 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-               
-                <div className="col-6">
-                  <div className="row">
-                     <p className="author-name">
-                         S.M Rakibul Huda
+          
+            
+            <nav>
+              
+              <input type="checkbox" id="check"/>
+              <label htmlFor="check" className="checkbtn">
+              <FontAwesomeIcon icon={faBars} />
+              </label>
+              <label >
+              <div >
+                  <div className="row author-name">
+                     <p className="name">
+                         S.M Rakibul Huda/
                      </p>
-                     <p>/</p>
-                     <p>Software Engineer</p>
+                     
+                     <p className="profession">Software Engineer</p>
                   </div>
                        
                </div>
-               <div className="col-6">
-               <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Resume</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Projects</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-      
-    </ul>
-  </div>
-               
-               
-               </div>
-               
+              </label>
+              <ul>
                 
+                <li><NavLink to="/home">Home</NavLink></li>
+                <li><NavLink to="/resume">Resume</NavLink></li>
+                <li><NavLink to="/projects">Projects</NavLink></li>
+                <li><NavLink to="/Contact">Contact</NavLink></li>
+              </ul>
             </nav>
+            
+            
+           
+         
             );
     }
 }
