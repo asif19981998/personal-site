04f38f7aaa543b4cbf7ae0d63f1ecbe1695e4913
asif19981998 <rakibulasif1998@gmail.com>
2021-03-25 22:04:18 +0600

    complete home page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Projects from './main-component/projects';
 import Contact from './main-component/contact';
 import { Route } from 'react-router';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCertificate } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <React.Fragment>
@@ -23,7 +25,39 @@ function App() {
       <Route path="/contact" component={Contact}></Route>
     </body>
     <footer>
-      <div>footer</div>
+      <div>
+        <div className="row">
+          <div className="col-6 col-md-6">
+            <p>Copyright @2021 By S.M Rakibul Huda</p>
+          </div>
+          <div className="col-6 col-md-6">
+            <div className="row">
+              <div className="col-4 col-md-4">
+              <p>Call<br></br>
+            01706634346
+            </p>
+              </div>
+              <div className="col-4 col-md-4">
+              <p>Gamil<br></br>
+            rakibulasif1998@gmail.com
+            </p>
+              </div>
+              <div className="col-4 col-md-4">
+              <p>Follow<br></br>
+            <FontAwesomeIcon icon={faCertificate}/>
+            <FontAwesomeIcon icon={'twitter'}/>
+            <FontAwesomeIcon icon={'linkedin'}/>
+            <FontAwesomeIcon icon={'instagram'}/>
+            
+            </p>
+              </div>
+            
+           
+            
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
     </React.Fragment>
     
--- src/main-component/home.jsx
+++ src/main-component/home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
+import authorImg from "../image/author.jpg"
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -9,8 +10,37 @@ class Home extends Component {
         return ( 
         <div className="row">
             
-                <div className="col-12 col-md-6 left-side">Left</div>
-                <div className="col-12 col-md-6">Right</div>
+                <div className="col-12 col-md-6 left-side">
+                  <div className="author-detail">
+                  <div class="card" >
+  <img className="card-img-top" src={authorImg} alt="Card image"/>
+  <div className="card-body">
+    <h4  className="card-title"  >S.M Rakibul Huda</h4>
+    <hr ></hr>
+    <p>Software Engineer</p>
+   
+    
+  </div>
+  
+</div>
+
+                  </div>
+                </div>
+                
+                
+                
+                <div className="col-12 col-md-6 right-side">
+                    <h1>About Me</h1>
+                    <div>
+                       <NavLink to="/resume"><button className="btn btn-primary">Resume</button></NavLink> 
+                        <NavLink to="/projects"><button className="btn btn-primary">Project</button></NavLink>
+                    </div>
+                    <p>I have always tried to learn new things and love to face challenges.
+I am interested in web programming, e.g developing web apps. Artificial Intelligence(AI) based solution
+is also fascinated me where I also working on that. 
+As a full-stack developer try to update me with technologies and try to acquire more bits of knowledge.
+I am a former Junior Software Engineer in AITS where I was working with Angular for one year.</p>
+                </div>
             
            
            
